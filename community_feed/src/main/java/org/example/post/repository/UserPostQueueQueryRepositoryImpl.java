8d72092dafad4fbbad5f1f2ce2838ad8cf1fa91b
mbc2579 <mbc2579@naver.com>
2025-09-01 21:15:43 +0900

    admin 템플릿 적용 및 기본 설정

---
+++ community_feed/src/main/java/org/example/post/repository/UserPostQueueQueryRepositoryImpl.java
@@ -0,0 +1,15 @@
+package org.example.post.repository;
+
+import java.util.List;
+import org.example.post.repository.post_queue.UserPostQueueQueryRepository;
+import org.example.post.ui.dto.GetPostContentResponseDto;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserPostQueueQueryRepositoryImpl implements UserPostQueueQueryRepository {
+
+    @Override
+    public List<GetPostContentResponseDto> getContentResponse(Long userId, Long lastPostId) {
+        return List.of();
+    }
+}
---
+++ community_feed/src/main/java/org/example/post/repository/UserQueueRedisRepositoryImpl.java
@@ -0,0 +1,25 @@
+package org.example.post.repository;
+
+import java.util.List;
+import org.example.post.repository.entity.post.PostEntity;
+import org.example.post.repository.post_queue.UserQueueRedisRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserQueueRedisRepositoryImpl implements UserQueueRedisRepository {
+
+    @Override
+    public void publishPostToFollowingUserList(PostEntity postEntity, List<Long> userIdList) {
+
+    }
+
+    @Override
+    public void publishPostListToFollowerUser(List<PostEntity> postEntityList, Long userId) {
+
+    }
+
+    @Override
+    public void deleteDeleteFeed(Long userId, Long authorId) {
+
+    }
+}
