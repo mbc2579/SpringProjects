53d1a4bc5e580a1b359e63ec4bae8484a0ade6ce
mbc2579 <mbc2579@naver.com>
2025-09-10 19:08:56 +0900

    불필요한 import 제거

--- chat-service/src/main/java/com/sparta/chatservice/configs/WebSocketConfiguration.java
+++ chat-service/src/main/java/com/sparta/chatservice/configs/WebSocketConfiguration.java
@@ -3,7 +3,6 @@ package com.sparta.chatservice.configs;
 import com.sparta.chatservice.handlers.WebSocketChatHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
